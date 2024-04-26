6423d038dcdb9271ad955439b225516181f3efb8
Timokrut <potapov.tim14@gmail.com>
2024-04-27 00:45:37 +0300

    V2.5

--- collectdata.py
+++ collectdata.py
@@ -1,17 +1,10 @@
 import re
-import requests # type: ignore
-from bs4 import BeautifulSoup as bs # type: ignore
-import numpy as np
+import requests
+from bs4 import BeautifulSoup as bs
 import pyperclip
+import os
 # HexagonalDipyramid
 
-
-name = input('Напишите название фигуры(как нас сайте): ')
-url = f'http://dmccooey.com/polyhedra/{name}.txt'
-page = requests.get(url)
-soup = bs(page.text, 'html.parser')
-
-data = page.text
 
 def extract_numbers(string):
     return [int(num) for num in re.findall(r'-?\d+\.?\d*', string)]
@@ -28,14 +21,29 @@ def remake_array(arr):
             new_arr.extend(new_subarr)
     return new_arr
 
+def convert_text(input_text):
+    changed = ['C_{']
+    for i in range(0, len(input_text)):
+        flag = True
+        if input_text[i] == '-' and flag == True:
+            changed.insert(0, '-')
+            flag = False
+
+        if input_text[i] == 'C' or input_text[i] == '-':
+            continue
+        changed.append(input_text[i])
+    changed.append('}')
+    return "".join(changed)
+
 def collect_data(data):
     i = 0
     Constants = dict()
     while (True):
         patternC = rf'C{i} = ([\d\.]+)'
         value = re.search(patternC, data)
+
         if value:
-            Constants[f"C_{i}"] = float(value.group(1))
+            Constants["C_{" + str(i) + "}"] = float(value.group(1))
             i += 1
         else:
             break
@@ -57,15 +65,20 @@ def collect_data(data):
             vertices.append([float(x.strip()) if x.isnumeric() else x.strip() for x in (line.split('=')[1].strip()[1:-1].split(', '))])  
         elif line.startswith('{'):
             faces.append(extract_numbers(line))
+    
+    for points in vertices:
+        for i in range(len(points)):
+            points[i] = convert_text(points[i])
+
     return vertices, faces, Constants
 
-vertices, faces, Constants = collect_data(data)
+# def analyze_constants(data):
 
 
 def generate_desmos_code(vertices, edges, constants):
     constants_code = "\n".join([f"{c} = {constants[c]}" for c in constants])
 
-    vertices_code = ", ".join([f'({vertex[0].replace("C", "C_")}, {vertex[1].replace("C", "C_")}, {vertex[2].replace("C", "C_")})' for vertex in vertices])
+    vertices_code = ", ".join([f'({vertex[0]}, {vertex[1]}, {vertex[2]})' for vertex in vertices])
     
     edges_code = ", ".join([f"({(edge[0] + 1)}, {str(edge[1] + 1)}, {edge[2] + 1})" for edge in edges])
 
@@ -73,8 +86,26 @@ def generate_desmos_code(vertices, edges, constants):
     desmos_code = f"{constants_code}\nP = [{vertices_code}]\nF = [{edges_code}]\ntriangle (P[F.x], P[F.y], P[F.z])"
     return desmos_code
 
-new_edges = remake_array(faces)
+if __name__ == "__main__":
+    while True:
+        name = input('Напишите название фигуры(как нас сайте): ')
+        url = f'http://dmccooey.com/polyhedra/{name}.txt'
+        page = requests.get(url)
+        soup = bs(page.text, 'html.parser')
 
-desmos_code = generate_desmos_code(vertices, new_edges, Constants)
-pyperclip.copy(desmos_code)
-print("Координаты скопированы в буфер обмена. Зайдите в Desmos и нажмите CTRL + V")
+
+        data = page.text.replace('  ', ' ')
+
+        vertices, faces, Constants = collect_data(data)
+
+        new_edges = remake_array(faces)
+
+        desmos_code = generate_desmos_code(vertices, new_edges, Constants)
+        pyperclip.copy(desmos_code)
+        print("Координаты скопированы в буфер обмена. Зайдите в Desmos и нажмите CTRL + V")
+
+        print("Хотите ввести еще одно название(Нажмите Enter)?(Если нет - напишите n)")
+        reply = input()        
+        if reply.lower() == 'n':
+            break
+        os.system("cls")
