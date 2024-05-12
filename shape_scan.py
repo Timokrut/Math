1d834fbb9bae1a8e4e269db22f8d073c680b90d1
Timokrut <potapov.tim14@gmail.com>
2024-05-12 20:36:03 +0300

    idk

--- shape_scan.py
+++ shape_scan.py
@@ -1,36 +1,20 @@
 from sympy import symbols, Eq, solve
 
-C0 = 0.7071067811865475244008443621048
-
-Verticies = [[0.0, 0.0,  C0], [0.0, 0.0, -C0], [ C0, 0.0, 0.0], [-C0, 0.0, 0.0], [0.0,  C0, 0.0], [0.0, -C0, 0.0]]
-
-F = [[0, 2, 4],[0, 4, 3],[0, 3, 5],[0, 5, 2],[1, 2, 5],[1, 5, 3],[1, 3, 4],[1, 4, 2]]
-
 def distance(point_A: list[float], point_B: list[float]) -> float:
     return ((point_A[0] - point_B[0])**2 + (point_A[1] - point_B[1])**2 + (point_A[2] - point_B[2])**2)**0.5
 
 #                  A - V0                B - V2                V - 4                 I[len(I) - 2]         I[len(I) - 1]
-def count_solution(point_A: list[float], point_B: list[float], point_C: list[float], point_D: list[float], point_E: list[float], I : list[float]) -> None:
+def count_solution(point_A: list[float], point_B: list[float], point_C: list[float], point_D: list[float], point_E: list[float]) -> None:
     x, y = symbols('x y')
     eq1 = Eq((x - point_D[0])**2 + (y - point_D[1])**2, distance(point_A, point_C)**2)
     eq2 = Eq((x - point_E[0])**2 + (y - point_E[1])**2, distance(point_B, point_C)**2)
     sol_dict = solve((eq1,eq2), (x, y))
-    # print(f' (x - {point_D[0]})^2 + (y - {point_D[1]})^2 = distance ({point_A}, {point_C})^2 // {distance(point_A, point_C)}^2 ')
-    # print(f' (x - {point_E[0]})^2 + (y - {point_E[1]})^2 = distance ({point_B}, {point_C})^2 // {distance(point_B, point_C)}^2 ')
 
-    # print()
+    answ = []
+    for i in sol_dict:
+        answ.append(list(i) + [0])
     
-    solution = sol_dict[1] if sol_dict[1][0] > sol_dict[0][0] else sol_dict[0]
-
-    # print(f'solution 1 - {sol_dict[0]}')
-    # print(f'solution 2 - {sol_dict[1]}')
-    # print(f'pick - {list(solution) if solution in I else list(sol_dict[0] if sol_dict[0] in I else sol_dict[1])}')
-    # print()
-        
-
-
-    return list(solution) if solution in I else list(sol_dict[0] if sol_dict[0] in I else sol_dict[1])
-
+    return answ
 
 def find_sim(face_A: list[int], face_B: list[int]):
     counter = 0
@@ -68,34 +52,3 @@ def find_number(aid, goodP):
     for i in aid.keys():
         if i == goodP:
             return aid[i]
-
-
-# INIT
-if __name__ == '__main__':
-    I = [[0, 0], [0, distance(Verticies[0], Verticies[2])]]
-    I.append(count_solution(Verticies[0], Verticies[2], Verticies[4], [0, 0], [0, distance(Verticies[0], Verticies[2])], I))
-
-    medical_aid = {
-    0 : I[0],
-    2 : I[1], 
-    4 : I[2]
-}
-
-    for counter in range(len(F)):
-        vericies, good_verticies = point_max_comp(F, counter)
-        for count, good_point in enumerate(good_verticies):
-            # print(f'good point - {good_point}')
-            # print(f'find_num_0 - {find_number(medical_aid, good_point[0])}')
-            # print(f'find_num_1 - {find_number(medical_aid, good_point[1])}')
-
-            I.append(count_solution(Verticies[good_point[0]], Verticies[good_point[1]], Verticies[vericies[count]], find_number(medical_aid, good_point[0]), find_number(medical_aid, good_point[1]), I))
-            
-            medical_aid[vericies[count]] = I[-1]
-            
-            # print(I)
-            # print(f'verticies - {vericies}')
-            
-            # print(f'medical aid - {medical_aid}')
-
-            # print()
-            # print()
--- test.py
+++ test.py
@@ -2,7 +2,9 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 from collectdata import remake_array, extract_numbers, convert_text
-from shape_scan import distance, count_solution
+from shape_scan import count_solution, distance, find_sim, point_max_comp, find_number
+
+EPSILON = 10e-8
 
 def collect_data(data: str):
     i = 0
@@ -46,7 +48,8 @@ def collect_data(data: str):
     return vertices, faces, Constants
 
 
-name = input('Напишите название фигуры(как нас сайте): ')
+# name = input('Напишите название фигуры(как нас сайте): ')
+name = 'TrigonalTrapezohedron'
 url = f'http://dmccooey.com/polyhedra/{name}.txt'
 page = requests.get(url)
 soup = bs(page.text, 'html.parser')
@@ -58,14 +61,86 @@ vertices, faces, Constants = collect_data(data)
 first_face = faces[0]
 
 new_edges = remake_array(faces)
-
-
-
 amount_of_verticies = {}
-print(vertices)
 
 for i in vertices:
-    amount_of_verticies[i] = 0
+    amount_of_verticies[vertices.index(i)] = 0
 
-print(amount_of_verticies)    
+from typing import Dict
+
+def check_solution(solution: list[float], I: list[float], amount_of_verticies: Dict[int,int], number_of_vertice: int, compare_numb_to_I: list[int]) -> int:
+    if len(solution) == 0:
+        return -1
+    if len(solution) == 1: 
+        if solution[0] in I:
+            return -1
+        else:
+            if amount_of_verticies[number_of_vertice] >= 1:
+                idx = compare_numb_to_I.find(number_of_vertice)
+                d1 = distance(I[idx], [0, 0, 0])
+                d2 = distance(solution[0], [0, 0, 0])
+                if d1 == d2:
+                    I.append(solution[0])
+                    amount_of_verticies[number_of_vertice] += 1
+                else:
+                    return -1     
+            else:
+                I.append(solution[0]) 
+                amount_of_verticies[number_of_vertice] += 1
+
+    if len(solution) == 2:
+        flag = 0
+        for i in solution:
+            if i in I:
+                flag += 1
+                continue
+        
+            if amount_of_verticies[number_of_vertice] >= 1:
+                idx = compare_numb_to_I.index(number_of_vertice)
+                d1 = distance(I[idx], [0, 0, 0])
+                d2 = distance(i, [0, 0, 0])
+                if d1 == d2:
+                    I.append(i)
+                    amount_of_verticies[number_of_vertice] += 1
+                else:
+                    flag += 1
+                    continue   
+            else:
+                I.append(i)
+                amount_of_verticies[number_of_vertice] += 1
+                return 0
+        else:
+            return -1 if flag == 2 else 0
+            
+    
+if __name__ == '__main__':
+    I = [[0, 0, 0]]
+    compare_numb_to_I = [i for i in new_edges[0]]
+    I.append([0, distance(vertices[0], vertices[compare_numb_to_I[1]]), 0])
+    check_solution(count_solution(vertices[0], vertices[compare_numb_to_I[1]], vertices[compare_numb_to_I[2]], I[0], I[1]), I, amount_of_verticies, 5, compare_numb_to_I)
+
+    for counter in range(len(new_edges)):
+        verts, good_verticies = point_max_comp(new_edges, counter)
+        for count, good_point in enumerate(good_verticies):
+            idx_0 = []
+            # if amount_of_verticies[good_point[0]] > 1:
+            for i in range(len(compare_numb_to_I)):
+                if compare_numb_to_I[i] == good_point[0]:
+                    idx_0.append(i)
+            
+            idx_1 = []                    
+            # if amount_of_verticies[good_point[1]] > 1:
+            for i in range(len(compare_numb_to_I)):
+                if compare_numb_to_I[i] == good_point[1]:
+                    idx_1.append(i)
+
+            flag = True
+            for i in idx_0:
+                if flag == False:
+                    break
+                for j in idx_1:
+                    if flag and check_solution(count_solution(vertices[good_point[0]], vertices[good_point[1]], vertices[verts[count]], I[i], I[j]), I, amount_of_verticies, verts[count], compare_numb_to_I) == 0:
+                        flag = False
+                        break
+    print(I)
 
